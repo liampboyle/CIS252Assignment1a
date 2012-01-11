e7177a2ef138a7ac3f49e6c00f01bbce75299664
Liam Boyle <liampboyle@gmail.com>
2012-01-11 14:59:47 -0500

    first commit JCTC CIS 252 assignment

---
+++ Output1.cpp
@@ -0,0 +1,29 @@
+/* *********************************************************************
+ * Liam Boyle
+ * CIS 252-55Z1
+ * Assignment 1 Output1.cpp
+ * 11 Jan 2012
+ * *********************************************************************
+ */
+
+ #include <iostream>
+ #include <cstdlib>
+ using namespace std;
+
+int main()
+{
+	// Main function variable declarations
+	int creditHours;
+	double gradePointAverage;
+
+	// Main function code body
+	creditHours = 15;
+	gradePointAverage = 3.25;
+
+	// I/O sequence
+	cout << "The number of credit hours is:  " << creditHours << endl;
+	cout << "The grade point average is:  " << gradePointAverage
+		<< endl;
+
+	return 0;
+}
